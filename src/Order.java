d272d98a45482765959e949cd2f6dc8b5a597ffb
ggingembre@gmail.com <Sailing2010>
2017-03-11 11:29:10 +0200

    Commit 1: HW7 task 1

---
+++ src/Order.java
@@ -0,0 +1,92 @@
+import java.util.Currency;
+
+/**
+ * Created by Guillaume Gingembre on 11/03/2017.
+ */
+public class Order {
+
+    private long id;
+    private int price;
+    private Currency currency;
+    private String itemName;
+    private String shopIdentificator;
+
+    // constructor with all fields
+
+    public Order(long id, int price, Currency currency, String itemName, String shopIdentificator) {
+        this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.itemName = itemName;
+        this.shopIdentificator = shopIdentificator;
+    }
+
+    // equals and hash method
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Order)) return false;
+
+        Order order = (Order) o;
+
+        if (id != order.id) return false;
+        if (price != order.price) return false;
+        if (!currency.equals(order.currency)) return false;
+        if (!itemName.equals(order.itemName)) return false;
+        return shopIdentificator.equals(order.shopIdentificator);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + price;
+        result = 31 * result + currency.hashCode();
+        result = 31 * result + itemName.hashCode();
+        result = 31 * result + shopIdentificator.hashCode();
+        return result;
+    }
+
+
+    // get and set methods
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getShopIdentificator() {
+        return shopIdentificator;
+    }
+
+    public void setShopIdentificator(String shopIdentificator) {
+        this.shopIdentificator = shopIdentificator;
+    }
+}
